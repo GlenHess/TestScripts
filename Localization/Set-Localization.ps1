023a633984d7b56ca315ab761d1ae53bd2d2104a
GlenHess <51173285+GlenHess@users.noreply.github.com>
2023-10-27 10:40:27 -0400

    Update Set-Localization.ps1

--- Localization/Set-Localization.ps1
+++ Localization/Set-Localization.ps1
@@ -24,34 +24,34 @@ Foreach ($Req_Var in $Req_Vars.Keys)
 
 $currentLanguage = (Get-Culture).Name
 
-if($SetLanguage -ne $currentLanguage)
+if($LanguageRegionCode -ne $currentLanguage)
 {
     $OSInfo = Get-WmiObject -Class Win32_OperatingSystem
     $languagePacks = $OSInfo.MUILanguages
-    If($SetLanguage)
+    If($LanguageRegionCode)
     {
-        If($languagePacks.ToLower() -contains $SetLanguage.ToLower() -eq $False)
+        If($languagePacks.ToLower() -contains $LanguageRegionCode.ToLower() -eq $False)
         {
             # Handle language pack not matching - Language not installed
-            Write-Error "This language pack $SetLanguage is not installed. Please install it first."
+            Write-Error "This language pack $LanguageRegionCode is not installed. Please install it first."
         }
         Else
         {
             # Language installed, set it.
-            Set-Culture $SetLanguage
-            Set-WinSystemLocale $SetLanguage
-            Set-WinHomeLocation $(Get-GeoId($SetLanguage))
+            Set-Culture $LanguageRegionCode
+            Set-WinSystemLocale $LanguageRegionCode
+            Set-WinHomeLocation $(Get-GeoId($LanguageRegionCode))
             
             # Find all matching major language inputs
-            $LanguageInput = $languagePacks | Where-Object {$PSItem -match ($SetLanguage.Split('-')[0])}
+            $LanguageInput = $languagePacks | Where-Object {$PSItem -match ($LanguageRegionCode.Split('-')[0])}
             Set-WinUserLanguageList $LanguageInput -force
             RunElevated({New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft" -Name "Edge" -Force})
             RunElevated({New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "SpellcheckLanguage" -Force})
             New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Edge" -Force
-            RunElevated($({New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "ApplicationLocaleValue" -Value {0}} -f $SetLanguage))
-            RunElevated($({New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\SpellcheckLanguage" -Name 1 -Value {0}} -f $SetLanguage))
-            New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Edge" -Name "ApplicationLocaleValue" -Value $SetLanguage
-            New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Edge" -Name "DefinePreferredLanguages" -Value $SetLanguage
+            RunElevated($({New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "ApplicationLocaleValue" -Value {0}} -f $LanguageRegionCode))
+            RunElevated($({New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\SpellcheckLanguage" -Name 1 -Value {0}} -f $LanguageRegionCode))
+            New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Edge" -Name "ApplicationLocaleValue" -Value $LanguageRegionCode
+            New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Edge" -Name "DefinePreferredLanguages" -Value $LanguageRegionCode
             
             # Remove deprecated scheduled task
             if (Get-ScheduledTask -TaskName "Localization" -ErrorAction Ignore)
