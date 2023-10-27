1c9ab0e6ac4fdd61bc20766719c60338fb8d559d
GlenHess <51173285+GlenHess@users.noreply.github.com>
2023-10-27 11:42:43 -0400

    Update BootStrap.ps1

--- BootStrap.ps1
+++ BootStrap.ps1
@@ -11,6 +11,19 @@ $Bootstrap_Params = @{
     PostAction = {if ($Reboot) {shutdown -r -t 5}}
 }
 
+# Wait for Internet Connection
+$MaxRetries = 10
+$Retry = 0
+while (
+    -not (Test-Connection 'raw.githubusercontent.com' -count 1 -Quiet) -and
+    $Retry -lt $MaxRetries
+) {Start-Sleep -Seconds 2}
+
+If (-not (Test-Connection 'raw.githubusercontent.com' -count 1 -Quiet))
+{
+    throw('No connection available in 20 seconds.')
+}
+
 $Content = Invoke-RestMethod `
     -Method Get `
     -Uri "https://raw.githubusercontent.com/${Account}/${Repo}/main/${ScriptPath}" `
