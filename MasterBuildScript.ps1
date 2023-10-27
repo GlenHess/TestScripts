4806796fe2a688c9bab1011e4b55006b6352b1f9
GlenHess <51173285+GlenHess@users.noreply.github.com>
2023-10-27 09:23:27 -0400

    Update MasterBuildScript.ps1

--- MasterBuildScript.ps1
+++ MasterBuildScript.ps1
@@ -26,8 +26,28 @@ $content = Invoke-RestMethod `
 Invoke-Command -ScriptBlock $([scriptblock]::Create($content)) -NoNewScope
 
 # Get Script list
-$Scripts_List = Get-GitHubContents "GlenHess" "TestScripts" "Scripts.json"
+$Scripts_List = Get-GitHubContents $Account $Repo "Scripts.json"
 
 try {
+   # Execute scripts in order
+   $Bootstrap_Params.Scripts.Keys | Sort | %{
+       $ScriptName = $Bootstrap_Params.Scripts."$PSItem"
+       $ScriptItem = $Scripts_List | Where-Object {$PSItem.Name -eq $ScriptName}
+       if ($null -ne $ScriptItem)
+       {
+           $ScriptContent = Get-GitHubContents $Account $Repo $ScriptItem.Path
+           Write-Host "Executing Script: '${ScriptName}'"
+           Invoke-Command -ScriptBlock $([scriptblock]::Create($ScriptContent)) -NoNewScope
+           Write-Host "Completed Executing Script: '${ScriptName}'"
+       }
+       else {
+           Write-Error "Script: '${ScriptName}' not found"
+       }
+   }
 
-} catch {$_ | Out-String}
+   # Execute Post Action
+   Invoke-Command -ScriptBlock $Bootstrap_Params.PostAction -NoNewScope
+} catch {
+   $_ | Out-String
+   throw "Script failure"
+}
