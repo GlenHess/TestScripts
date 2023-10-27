ca69b04a2f837615177be550458f4dbcaeb74af6
GlenHess <51173285+GlenHess@users.noreply.github.com>
2023-10-27 08:07:02 -0400

    Update MasterBuildScript.ps1

--- MasterBuildScript.ps1
+++ MasterBuildScript.ps1
@@ -2,6 +2,20 @@
    This script is the first to be pulled down.
 #>
 
+# Required variables
+#   $Bootstrap_Params = @{
+#      Scripts = @{
+#         "0" = "Scriptname"
+#      }
+#      PostAction = {shutdown -r -t 5}
+#   }
+
+if (-not (Get-Variable -name Bootstrap_Params -ErrorAction Ignore))
+{
+  Write-Error "Required Variable missing: 'Bootstrap_Params'"
+}
+
+
 try {
 
 } catch {$_ | Out-String}
