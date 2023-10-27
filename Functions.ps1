a11cec36644902717f496f432192f89b115846b5
GlenHess <51173285+GlenHess@users.noreply.github.com>
2023-10-27 08:06:53 -0400

    Update Functions.ps1

--- Functions.ps1
+++ Functions.ps1
@@ -3,3 +3,27 @@
     This script will be invoked with no new scope.
 #>
 
+Function RunElevated($ScriptBLock)
+{
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell"
+    $newProcess.Arguments = $ScriptBlock
+    $newProcess.Verb = "runas"
+    [void][System.Diagnostics.Process]::Start($newProcess)
+}
+
+Function Get-GeoId($Name='*')
+{
+    $cultures = [System.Globalization.CultureInfo]::GetCultures('InstalledWin32Cultures') #| Out-GridView
+     foreach($culture in $cultures)
+    {
+       try{
+           $region = [System.Globalization.RegionInfo]$culture.Name
+           #Write-Host "00 :"$Name "|" $region.DisplayName "|" $region.Name "|" $region.GeoId "|" $region.EnglishName "|" $culture.LCID
+           if($region.Name -like $Name)
+           {
+                $region.GeoId
+           }
+       }
+       catch {}
+    }
+}
