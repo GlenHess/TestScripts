5d5a746f08cc28ec802de6fde0d933c58e587f2e
GlenHess <51173285+GlenHess@users.noreply.github.com>
2023-10-27 08:29:25 -0400

    Update MasterBuildScript.ps1

--- MasterBuildScript.ps1
+++ MasterBuildScript.ps1
@@ -15,6 +15,15 @@ if (-not (Get-Variable -name Bootstrap_Params -ErrorAction Ignore))
   Write-Error "Required Variable missing: 'Bootstrap_Params'"
 }
 
+# Import functions
+$Account = 'GlenHess'
+$Repo = 'TestScripts'
+$ScriptPath = 'Functions.ps1'
+$content = Invoke-RestMethod `
+              -Method Get `
+              -URI ([System.Web.HttpUtility]::UrlPathEncode("https://raw.githubusercontent.com/${Account}/${Repo}/main/${ScriptPath}")) `
+              -UseBasicParsing
+Invoke-Command -ScriptBlock $([scriptblock]::Create($content)) -NoNewScope
 
 try {
 
