d14da52f8a9d9258c18a846f76d6f7b7e2b5e24a
GlenHess <51173285+GlenHess@users.noreply.github.com>
2023-11-07 16:00:17 -0500

    Update BootStrap.ps1
    Convert to Private Repo

--- BootStrap.ps1
+++ BootStrap.ps1
@@ -1,6 +1,7 @@
 $Account = "<Enter GitHub Account name>"
 $Repo = "<Enter GitHub Repository name>"
 $ScriptPath = "<Enter GitHub Script Path>"
+$GitHubAuthToken = "<Enter GitHub Auth Token>"
 
 $LanguageRegionCode = '@lab.LanguageRegionCode'
 
@@ -24,9 +25,35 @@ If (-not (Test-Connection 'raw.githubusercontent.com' -count 1 -Quiet))
     throw('No connection available in 60 seconds.')
 }
 
+[System.Text.Encoding] $enc = [System.Text.Encoding]::UTF8
+
+$Content_Parameters = {
+    Param
+    (
+        [Parameter(Position=0)]$Account,
+        [Parameter(Position=1)]$repo,
+        [Parameter(Position=2)]$file
+    )
+    Add-Type -AssemblyName System.Web
+    @{
+        Method = 'Get'
+        URI = [System.Web.HttpUtility]::UrlPathEncode("https://api.github.com/repos/${Account}/${repo}/contents/${file}")
+        Header = @{
+            Accept = "application/vnd.github+json"
+            Authorization = "Bearer ${GitHubAuthToken}"
+            "X-GitHub-Api-Version" = "2022-11-28"
+        }
+    }
+}
+
+$parameters = &$Content_Parameters $Account $Repo $ScriptPath
+$Content = [scriptblock]::Create(($enc.GetString([Convert]::FromBase64String((Invoke-RestMethod @parameters).content))))
+
+<# Public Repo
 $Content = Invoke-RestMethod `
     -Method Get `
     -Uri "https://raw.githubusercontent.com/${Account}/${Repo}/main/${ScriptPath}" `
     -UseBasicParsing
+#>
 
 Invoke-Command -ScriptBlock $([scriptblock]::Create($Content)) -NoNewScope
